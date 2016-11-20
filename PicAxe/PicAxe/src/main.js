cfdb450e8b654c130071750aad90038503b64052
Mike Bundy <mike.bundy@gmail.com>
2016-11-19 23:06:02 -0600

    electron setup

---
+++ PicAxe/PicAxe/src/main.js
@@ -0,0 +1,22 @@
+﻿const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+app.on('ready', _ => {
+    mainWindow = new BrowserWindow({
+        height: 400,
+        width: 400
+    });
+
+    mainWindow.loadURL(path.join(__dirname, 'countdown.html'));
+
+    mainWindow.on('closed', _ => {
+        console.log('closed');
+        mainWindow = null;
+    });
+});
