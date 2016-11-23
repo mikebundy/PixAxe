588708162bf5784e57bd690e6fc1f7f89d98bdd1
Mike Bundy <mike.bundy@gmail.com>
2016-11-23 16:50:26 -0600

    ipc demo complete

--- PicAxe/PicAxe/src/main.js
+++ PicAxe/PicAxe/src/main.js
@@ -4,23 +4,37 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
+const countdown = require('./countdown');
+const ipc = electron.ipcMain;
 
-let mainWindow;
+const windows = [];
 
 app.on('ready', _ => {
-    mainWindow = new BrowserWindow({
-        height: 400,
-        width: 400
+    [1, 2, 3].forEach(_ => {
+        let win = new BrowserWindow({
+            height: 400,
+            width: 400
+        });
+
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'countdown.html'),
+            protocol: 'file',
+            slashes: true
+        }));
+
+        win.on('closed', _ => {
+            console.log('closed');
+            mainWindow = null;
+        });
+
+        windows.push(win);
     });
+});
 
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'countdown.html'),
-        protocol: 'file',
-        slashes: true
-    }));
-
-    mainWindow.on('closed', _ => {
-        console.log('closed');
-        mainWindow = null;
+ipc.on('countdown-start', _=> {
+    countdown(count => {
+        windows.forEach(win => {
+            win.webContents.send('countdown', count);
+        });
     });
 });
